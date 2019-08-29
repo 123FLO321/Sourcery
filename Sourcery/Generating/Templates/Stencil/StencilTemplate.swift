db265a337a7e99af150e635e6d989960380fb7ee
Florian Kostenzer <florian.kostenzer@icloud.com>
2019-08-29 17:22:41 +0200

    load Configuration.stencilTemplate correctly

--- Sourcery/Generating/Templates/Stencil/StencilTemplate.swift
+++ Sourcery/Generating/Templates/Stencil/StencilTemplate.swift
@@ -27,8 +27,7 @@ final class StencilTemplate: StencilSwiftKit.StencilSwiftTemplate, SourceryFrame
 
     private static func sourceryEnvironment(templatePath: Path? = nil) -> Stencil.Environment {
         
-        
-        //let ext = Configuration.stencilTemplate ?? Stencil.Extension()
+        let ext = Configuration.stencilTemplate ?? Stencil.Extension()
 
         let ext = Stencil.Extension()
         
